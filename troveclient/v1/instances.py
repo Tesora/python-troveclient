1db6cc32c0fe88fc0be5bc78d20673cc7be0577c
Greg Lucas <glucas@tesora.com>
2014-08-29 22:58:04 -0400

    Use 'replica' instead of 'slave'
    Replace the mysql-specific term 'slave' with the more general term
    'replica'.
    
    Change-Id: Id22d18a84a4ac104ff8af09959e8eb2ae0102e97
    Depends-On: Ief4bc83369fa3183a321d3e07b69b1801595ab15
    Closes-Bug: 1360310

--- troveclient/v1/instances.py
+++ troveclient/v1/instances.py
@@ -50,7 +50,7 @@ class Instances(base.ManagerWithFind):
     def create(self, name, flavor_id, volume=None, databases=None, users=None,
                restorePoint=None, availability_zone=None, datastore=None,
                datastore_version=None, nics=None, configuration=None,
-               slave_of=None):
+               replica_of=None):
         """Create (boot) a new instance."""
         body = {"instance": {
             "name": name,
@@ -77,8 +77,8 @@ class Instances(base.ManagerWithFind):
             body["instance"]["nics"] = nics
         if configuration:
             body["instance"]["configuration"] = configuration
-        if slave_of:
-            body["instance"]["slave_of"] = slave_of
+        if replica_of:
+            body["instance"]["replica_of"] = replica_of
 
         return self._create("/instances", body, "instance")
 
